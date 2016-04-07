e125a4fe5252fc32310a97a63612ba2a8232a477
noprom <tyee.noprom@qq.com>
2016-04-07 09:24:06 +0800

    define the stack and data segment

--- ass1/ass1.asm
+++ ass1/ass1.asm
@@ -1,3 +1,16 @@
 ;; Author: noprom <tyee.noprom@qq.com>
 ;; Data: 2016/4/1 2:24PM
 ;; Title: 实验一:分支程序设计
+
+;-------------------------栈段-------------------------;
+STACKSG SEGMENT STACK 'S'
+  DW 64 DUP('ST')
+STACKSG ENDS
+;-------------------------数据段-----------------------;
+datasg segment
+  A dw 0
+  B dw 0
+  FLAG dw 0
+  MSGA dw 'Please input A: ', '$'
+
+datasg ends
