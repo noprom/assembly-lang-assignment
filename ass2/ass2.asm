0a7cde0ec0ac630333c64c51c2555757cbba2f33
noprom <tyee.noprom@qq.com>
2016-04-08 09:23:15 +0800

    output array message

--- ass2/ass2.asm
+++ ass2/ass2.asm
@@ -12,13 +12,26 @@ stacksg ends
 datasg segment
   arrA db 1, 9, 2, 3, 7, 8, 4, 5, 10, 6, 20, 19, 18, 13, 14, 15, 32, 96, 193, 132     ;数组A
   ;db '$'
-  ctA equ ($-arrA)                              ;数组A长度
-  indexA db 0                                   ;输出数组A的下标
+  ctA equ ($-arrA)                              ;数组A元素个数
+  indexA db 0                                   ;输出数组A时的下标
 
   arrB db 3, 7, 9, 8, 1, 55, 33, 22, 10, 19, 21, 35, 60, 31, 14, 15, 23, 69, 93, 172 ;数组B
   ;db '$'
-  ctB equ ($-arrB)                              ;数组B长度
-  indexB db 0                                   ;输出数组B的下标
+  ctB equ ($-arrB)                              ;数组B元素个数
+  indexB db 0                                   ;输出数组B时的下标
+
+  arrC db 40 dup(?)                             ;数组C
+  ctC db 0                                      ;数组C元素个数,初始状态为0
+  indexC db 0                                   ;输出数组C时的下标
+
+  flag db 0                                     ;输出0判断flag
+
+  ;下面是输出的字符串常量
+  arrABeforeSortMsg  db 'Before sort, array A: ', '$'  ;输出'Before sort, array A: '
+  arrBBeforeSortMsg  db 'Before sort, array B: ', '$'  ;输出'Before sort, array B: '
+  arrAAfterSortMsg   db 'After sort, array A: ', '$'   ;输出'After sort, array A: '
+  arrBAfterSortMsg   db 'After sort, array B: ', '$'   ;输出'After sort, array B: '
+  arrCContentMsg     db 'The content of array C:','$'  ;输出'The content of array C: '
 
 datasg ends
 ;-------------------------代码段-----------------------;
@@ -26,7 +39,11 @@ codesg segment
   assume cs: codesg, ss: stacksg, ds: datasg    ;指定寄存器的关系
 ;-------------------------主过程-----------------------;
 main proc far
+  mov ax, datasg
+  mov ds, ax
+;----------------------输出A原来的值--------------------;
 
+;----------------------输出B原来的值--------------------;
 main endp
 codesg ends
 end main
